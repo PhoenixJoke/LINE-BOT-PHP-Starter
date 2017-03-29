5d5900e4fa6ec24c180d041057c0ef72f5aa6c08
PhoenixJoke <Phoenix_joke@yahoo.com>
2017-03-29 10:35:05 +0700

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -23,8 +23,8 @@ if (!is_null($events['events'])) {
 			];*/
 			$messages = [
 				'type' => 'text',
-				'text' => "ทดสอบข้อความตอบกลับภาษาไทย"
-			]
+				'text' => 'ทดสอบข้อความตอบกลับภาษาไทย'
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
