7d23d8a49b66145518781c080593e18a7550e84b
PhoenixJoke <Phoenix_joke@yahoo.com>
2017-03-29 10:40:30 +0700

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -13,7 +13,21 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			//$text = $event['message']['text'];
-			$text = 'ทดสอบภาษาไทย';
+			//$text = 'ทดสอบภาษาไทย';
+			
+			// connect to 142 for get reply message
+			$ch = curl_init("http://app3.shinee.com:85/Line/mybot_push.php");
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			
+			$text = $result;
+			// end connect
+			
 			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
